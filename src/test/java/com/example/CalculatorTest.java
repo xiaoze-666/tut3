3f0a9431168609fd5243a63757c207769411240c
fang hanze <1162801113@qq.com>
2026-09-22 15:54:32 +0800

    Add failing testSubtraction test

--- src/test/java/com/example/CalculatorTest.java
+++ src/test/java/com/example/CalculatorTest.java
@@ -10,4 +10,10 @@ public class CalculatorTest {
         Calculator c = new Calculator();
         assertEquals(6, c.add(4, 2));
     }
+
+    @Test
+    public void testSubtraction() {
+        Calculator c = new Calculator();
+        assertEquals(2, c.subtract(4, 2));
+    }
 }
